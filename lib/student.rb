34d1b2db068588708fb5a16989b68f0a93150990
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-20 15:01:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student < User
   attr_reader :knowledge
   def initialize
-    @knowledge = Array.new
+    knowledge = Array.new
   end
 
   def learn(knowledge_string)
-    @knowledge << knowledge_string
+    knowledge << knowledge_string
   end
 
 end
